2c1fa299b3a8001d9f4ced0fb34d08644196681a
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-07-29 06:30:51 +0200

    added timestamp constants. Created a new relationship courses-entities

--- app/Models/Entity.php
+++ app/Models/Entity.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Entity extends Model
 {
-    public $timestamps = false;
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    public function courses()
+    {
+        return $this->hasOne(Course::class, 'entity_id', 'id');
+    }
 }
