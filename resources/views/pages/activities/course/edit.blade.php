41a67113ba538bf19f299cdb80053b30b09987ab
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-07-31 00:54:31 +0200

    Removed unnecessary parameters

--- resources/views/pages/activities/course/edit.blade.php
+++ resources/views/pages/activities/course/edit.blade.php
@@ -5,12 +5,6 @@
   <form method="POST" action="{{route('course.update',$course->id)}}">
     @csrf
     {{ method_field('PUT') }}
-    @include("pages.activities.course._form",
-    ['buttonText'=>"Edit",
-    'accPerId'=>$course->accounting_period_id,
-    'semesterId'=>$course->semester_id,
-    'termId'=>$course->term_id,
-    'languageId'=>$course->language_id,
-    'nameOfSubject'=>$course->name_of_subject])
+    @include("pages.activities.course._form",['buttonText'=>"Edit"])
   </form>
 @endsection
