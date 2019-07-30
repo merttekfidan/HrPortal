1d6b9934c8ee5adbfb87995bec56d55590783119
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-07-31 00:54:51 +0200

    Removed space

--- resources/views/pages/activities/course/create.blade.php
+++ resources/views/pages/activities/course/create.blade.php
@@ -1,7 +1,6 @@
 @extends('theme.layout')
 @section('formName','Course')
 @section('content')
-
   <form method="post" action="{{route('course.store')}}">
     @csrf
     @include("pages.activities.course._form",['buttonText'=>"Create"])
