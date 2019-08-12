243ccf8c9be86c72102be5eebd4fe59becb28685
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-08-12 08:07:18 +0200

    Change css source amd display two buttons side by side

--- resources/views/pages/activities/course/edit.blade.php
+++ resources/views/pages/activities/course/edit.blade.php
@@ -2,9 +2,14 @@
 @section('formName','Course')
 @section('content')
 
-  <form method="POST" action="{{route('course.update',$course->id)}}">
+  <form class="form-edit" method="POST" action="{{route('course.update',$course->id)}}">
     @csrf
     {{ method_field('PUT') }}
     @include("pages.activities.course._form",['buttonText'=>"Edit"])
+  </form>
+  <form class="form-delete" method="POST" action="{{route('course.delete',$course->id)}}">
+    @csrf
+    {{method_field('delete')}}
+    <button type="submit" class="btn btn-danger">Delete</button>
   </form>
 @endsection
